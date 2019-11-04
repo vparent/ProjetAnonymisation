e5b769a5da76377adadddc994f64786c4a1a40f6
tdejean <theotim.dejean@insa-cvl.fr>
2019-11-04 18:03:01 +0100

    generalize to a file given in argument and the newline problem

---
+++ month_spliter.py
@@ -0,0 +1,24 @@
+import csv
+
+with open('ground_truth.csv') as truth:
+    reader = csv.DictReader(truth)
+    row1 = reader.__next__()
+    temp = row1['date'][0:7]
+    print(temp)
+    i = 1
+    month = []
+    for row in reader:
+        if row['date'][0:7] == temp:
+            month.append(row)
+        else:
+            with open("ground_truth_mounth_"+str(i)+".csv", 'w') as writer:
+                fields = ['id_user','date','hours','id_item','price','qty']
+                mwriter = csv.DictWriter(writer, delimiter=',', fieldnames=fields)
+                mwriter.writeheader()
+                for r in month:
+                    mwriter.writerow(r)
+            month=[row]
+            temp = row['date'][0:7]
+            i += 1
+
+
